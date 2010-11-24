33184652558fa360228d2d5e303f3a9293d5a015
Ryan Farley <ryan@farleyzone.com>
2010-11-24 15:16:07 -0700

    Added test task assembly

---
+++ TestTask/TestTask.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using FX.Services.Components;
+using System.IO;
+using System.Web;
+
+namespace FX.Tasks
+{
+	public class TestTask : TaskBase
+	{
+		public override void Execute(System.Xml.XmlNode configuration)
+		{
+			using (var outfile = new StreamWriter(Path.Combine(HttpContext.Current.Server.MapPath("/"), "_TestTask_Log.txt")))
+			{
+				outfile.WriteLine(DateTime.Now.ToString() + " TestTask executed");
+				outfile.Close();
+			}
+		}
+	}
+}
